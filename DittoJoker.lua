997919e70c322daad95a859812a14344d86ced23
UltraCboy <ultracboy@gmail.com>
2024-06-01 15:12:58 -0400

    Ditto procs on Cryptid used

--- DittoJoker.lua
+++ DittoJoker.lua
@@ -47,6 +47,7 @@ SMODS.Joker{
         return {vars = {card.ability.extra.increment, card.ability.extra.current}}
 	end,
 	calculate = function(self, card, context)
+		-- Scoring
 		if context.cardarea == G.jokers and context.joker_main and card.ability.extra.current > 1 then
 			return {
 				Xmult_mod = card.ability.extra.current,
@@ -57,13 +58,40 @@ SMODS.Joker{
 				},
 			}
 		end
-		if context.using_consumeable then
+		if context.using_consumeable and not context.blueprint then
+			-- Using Death
 			if context.consumeable.ability.name == "Death" then
 				card.ability.extra.current = card.ability.extra.current + card.ability.extra.increment
 				card_eval_status_text(card, 'extra', nil, nil, nil, 
 					{message = localize{type = 'variable', key = 'a_xmult', vars = {card.ability.extra.current}}}
 				)
 			end
+			-- Using Cryptid
+			if context.consumeable.ability.name == "Cryptid" then
+				card.ability.extra.current = card.ability.extra.current + 2 * card.ability.extra.increment
+				card_eval_status_text(card, 'extra', nil, nil, nil, 
+					{message = localize{type = 'variable', key = 'a_xmult', vars = {card.ability.extra.current}}}
+				)
+			end
+		end
+		-- Creating things on shops ending - TESTING ONLY
+		if context.ending_shop then
+			G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+            G.E_MANAGER:add_event(Event({
+                trigger = 'before',
+                delay = 0.0,
+                func = (function()
+                        local card = create_card('Spectral',G.consumeables, nil, nil, nil, nil, 'c_cryptid', 'dit')
+                        card:add_to_deck()
+                        G.consumeables:emplace(card)
+                        G.GAME.consumeable_buffer = 0
+                    return true
+                end)}))
+            return {
+                message = localize('k_plus_spectral'),
+                colour = G.C.SECONDARY_SET.Spectral,
+                card = self
+            }
 		end
 	end,
     atlas = "ditto"
