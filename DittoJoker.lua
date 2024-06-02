685749e66d3e63aa089bb1382d99fbce237b89db
UltraCboy <ultracboy@gmail.com>
2024-06-02 16:45:04 -0400

    Ditto procs on DNA use (with some assumptions)

--- DittoJoker.lua
+++ DittoJoker.lua
@@ -1,6 +1,6 @@
 --- STEAMODDED HEADER
 --- MOD_NAME: Ditto
---- MOD_ID: DITT
+--- MOD_ID: DittoJoker
 --- MOD_AUTHOR: [UltraCboy]
 --- MOD_DESCRIPTION: Ditto Joker
 --- LOADER_VERSION_GEQ: 1.0.0
@@ -31,7 +31,7 @@ SMODS.Joker{
     cost = 7,
     config = {
 		extra = {
-			increment = 0.3, current = 1
+			increment = 0.3, current = 1, dna_maybe = false, spawn_goodies = false
 		}
 	},
     loc_txt = {
@@ -48,15 +48,30 @@ SMODS.Joker{
 	end,
 	calculate = function(self, card, context)
 		-- Scoring
-		if context.cardarea == G.jokers and context.joker_main and card.ability.extra.current > 1 then
-			return {
-				Xmult_mod = card.ability.extra.current,
-				message = localize {
-					type = 'variable',
-					key = 'a_xmult',
-					vars = {card.ability.extra.current}
-				},
-			}
+		if context.cardarea == G.jokers then
+			card.ability.extra.dna_maybe = true
+			if context.joker_main then
+				card.ability.extra.dna_maybe = false
+				if card.ability.extra.current > 1 then
+					return {
+						Xmult_mod = card.ability.extra.current,
+						message = localize {
+							type = 'variable',
+							key = 'a_xmult',
+							vars = {card.ability.extra.current}
+						},
+					}
+				end
+			end
+		end
+		-- Using DNA
+		if context.playing_card_added and not context.blueprint and not self.getting_sliced 
+			and G.GAME.current_round.hands_played == 0 and card.ability.extra.dna_maybe
+			and context.cards and context.cards[1] then
+			card.ability.extra.current = card.ability.extra.current + #context.cards * card.ability.extra.increment
+			card_eval_status_text(card, 'extra', nil, nil, nil, 
+				{message = localize{type = 'variable', key = 'a_xmult', vars = {card.ability.extra.current}}}
+			)
 		end
 		if context.using_consumeable and not context.blueprint then
 			-- Using Death
@@ -75,7 +90,20 @@ SMODS.Joker{
 			end
 		end
 		-- Creating things on shops ending - TESTING ONLY
-		if context.ending_shop then
+		if card.ability.extra.spawn_goodies and context.ending_shop then
+			-- Create a Death Card
+			G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+            G.E_MANAGER:add_event(Event({
+                trigger = 'before',
+                delay = 0.0,
+                func = (function()
+                        local card = create_card('Tarot',G.consumeables, nil, nil, nil, nil, 'c_death', 'dit')
+                        card:add_to_deck()
+                        G.consumeables:emplace(card)
+                        G.GAME.consumeable_buffer = 0
+                    return true
+                end)}))
+			-- Create a Cryptid Card
 			G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
             G.E_MANAGER:add_event(Event({
                 trigger = 'before',
@@ -87,11 +115,18 @@ SMODS.Joker{
                         G.GAME.consumeable_buffer = 0
                     return true
                 end)}))
-            return {
-                message = localize('k_plus_spectral'),
-                colour = G.C.SECONDARY_SET.Spectral,
-                card = self
-            }
+			-- Create a DNA Joker
+			G.GAME.joker_buffer = G.GAME.joker_buffer + 1
+            G.E_MANAGER:add_event(Event({
+                func = function() 
+                    local card = create_card('Joker', G.jokers, nil, 0, nil, nil, 'j_dna', 'rif')
+                    card:add_to_deck()
+                    G.jokers:emplace(card)
+                    card:start_materialize()
+                    G.GAME.joker_buffer = 0
+                    return true
+                end}))
+			card.ability.extra.spawn_goodies = false
 		end
 	end,
     atlas = "ditto"
